527ec63058375d32af222902ec92c938c59e6ba3
Student <thommy.python.dev@gmail.com>
2023-02-02 13:26:59 +0300

    ///

---
+++ lesson2.py
@@ -0,0 +1,57 @@
+# # text = 'Hello World. ß Hello Python'
+# # # print(text.find('world'))
+# # # print(text.rfind('world'))
+# # # print(text.index('World'))
+# # # print(text.rindex('world'))
+# # # print(text.count('world', 8, 15))
+# # # print(text.partition('World'))
+# # # print(text.rpartition('World'))
+# # # print(text.islower())
+# # # print(text.isupper())
+# # # print(text.istitle())
+# #
+# # # text2 = text.capitalize()
+# # # text2 = text.swapcase()
+# # text2 = text.casefold()
+# # text3 = text.lower()
+# # print(text3)
+# # print(text2)
+#
+# # text = '.-,--hello---world--..,,'
+# # print(text.lstrip('.,-'))
+# # print(text.rstrip(',.-'))
+# # print(text.strip(',.-'))
+# # text = 'hello world'
+# # print(text.removeprefix('Hell'))
+# # print(text.removesuffix('rld '))
+#
+# # text = 'hello\tworld\tpython'
+# # text2 = 'hello python\tworld python\tpython python'
+# # print(text.expandtabs(15))
+# # print(text2.expandtabs(15))
+#
+# # print('hello'.center(13, '-'))
+# # print('hello'.zfill(4))
+# # print(bin(15)[2:].zfill(8))
+# # print('hello'.ljust(10, '-'))
+# # print('hello'.rjust(10, '-'))
+# # from string import ascii_lowercase
+#
+# # print(bin(13))
+# # print(bin(11))
+# # print(13 ^ 11)
+# # print(bin(6))
+# # print(~-12)
+# # print('hello world'[~-2])
+#
+# # print(15 >> 3)
+# text = 'hello python world'
+# print('python' in text)
+# print('python' not in text)
+# a = 4
+# b = (a // 2) * 2
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+
